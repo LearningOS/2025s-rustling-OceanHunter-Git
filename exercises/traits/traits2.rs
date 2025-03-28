33ab56a0f25e9afe697a6e0ac85e263da089a2eb
Your Name <you@example.com>
2025-03-28 22:01:33 +0800

    lifetime test iterator

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -17,7 +17,8 @@ trait AppendBar {
 // TODO: Implement trait `AppendBar` for a vector of strings.
 impl AppendBar for Vec<String>{
     fn append_bar(mut self) -> Vec<String>{
-        self.map(|x|{x+"Foo"})
+        self.push("Bar".to_string());
+        self
     }
 }
 
